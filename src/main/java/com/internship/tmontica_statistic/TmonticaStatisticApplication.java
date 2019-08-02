56d0880339e0192df69314c09cb775df7d506814
김형석 <vndtjd1217@naver.com>
2019-08-02 14:51:15 +0900

    STATISTIC PRJ CREATE

---
+++ src/main/java/com/internship/tmontica_statistic/TmonticaStatisticApplication.java
@@ -0,0 +1,13 @@
+package com.internship.tmontica_statistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmonticaStatisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmonticaStatisticApplication.class, args);
+    }
+//
+}
